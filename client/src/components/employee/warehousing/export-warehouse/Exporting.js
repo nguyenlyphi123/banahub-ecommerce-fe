78160cdad7af73efc136f2ea1c68b348a3eb5983
nguyenlyphi123 <nguyenlyphi247@gmail.com>
2023-05-08 10:11:09 +0700

    delete all file

--- client/src/components/employee/warehousing/export-warehouse/Exporting.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-
-import('./Exporting.css');
-
-export default function Exporting() {
-  return <Outlet />;
-}
--- client/src/contexts/constants.js
+++
@@ -1,5 +0,0 @@
-export const apiURL =
-  process.env.NODE_ENV === 'https://banahub-ecommerce-be.onrender.com/api';
-
-export const LOCAL_STORAGE_ACCESS_TOKEN = 'AccessToken';
-export const LOCAL_STORAGE_ACCESS_TOKEN_EMPLOYEE = 'EmployeeAccessToken';

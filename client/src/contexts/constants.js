f65523dd736d10a5172fd1d00797f684140cecb6
nguyenlyphi123 <nguyenlyphi247@gmail.com>
2023-05-08 09:30:06 +0700

    update apiURL

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,5 @@
 export const apiURL =
-  process.env.NODE_ENV !== 'production'
-    ? 'https://banahub-ecommerce-be.onrender.com/api'
-    : 'http://localhost:8000/api';
+  process.env.NODE_ENV === 'https://banahub-ecommerce-be.onrender.com/api';
 
 export const LOCAL_STORAGE_ACCESS_TOKEN = 'AccessToken';
 export const LOCAL_STORAGE_ACCESS_TOKEN_EMPLOYEE = 'EmployeeAccessToken';
